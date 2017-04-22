b92150c5bd5eee5c56d85737111b392b677a5d12
mdolata <mateuszdolata2@gmail.com>
2017-04-22 20:21:22 +0200

    Refactor of CurrencyController

--- src/main/java/com/example/controllers/CurrencyController.java
+++ src/main/java/com/example/controllers/CurrencyController.java
@@ -1,5 +1,8 @@
 package com.example.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,20 +12,31 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.Currency;
 
+
 /**
  * Created by mateu on 07.03.2017.
+ *
+ * Controller for currency convert
  */
 @RestController
 @RequestMapping("/currency")
 public class CurrencyController {
 
-    @RequestMapping("/")
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyController.class);
+
+    public String home2(){
+        return "Bad request";
+    }
+
+    @RequestMapping(value = {"/"," * ","/something"})
     public String home(){
         return "choose your value after slash";
     }
 
     @RequestMapping("/{number}")
     public Long multiplyByTwo(@PathVariable Long number){
+        logger.info("metoda multipleByTwo");
         return 2 * number;
     }
 
@@ -35,30 +49,38 @@ public class CurrencyController {
     public String exchangeValue(@PathVariable Long value,
                                 @RequestParam String from,
                                 @RequestParam String to){
-        String result;
-        try {
-            Currency currencyFrom = Currency.getInstance(from.toUpperCase());
-            result = value + currencyFrom.toString() + " equals ";
+        StringBuilder resultBuilder = new StringBuilder();
 
+        Currency currencyFrom;
+        Currency currencyTo;
+        BigDecimal rateExchange = new BigDecimal(Math.random() * 10 ).setScale(2, BigDecimal.ROUND_DOWN);
+        BigDecimal bigDecimalValue = new BigDecimal(String.valueOf(value));
+        try {
+            currencyFrom = Currency.getInstance(from.toUpperCase());
         } catch (NullPointerException e){
-            result = "Choose first currency!";
-            return result;
+            return "Choose first currency!";
         } catch (IllegalArgumentException e){
-            result = "Choose valid first currency!";
-            return result;
+            return "Choose valid first currency!";
         }
 
         try {
-            Currency currencyTo = Currency.getInstance(to.toUpperCase());
-            result += (BigDecimal.valueOf(Math.random()*10).setScale(2, BigDecimal.ROUND_DOWN).doubleValue()) * value + currencyTo.toString();
+            currencyTo = Currency.getInstance(to.toUpperCase());
         } catch (NullPointerException e){
-            result = "Choose second currency!";
-            return result;
+            return "Choose second currency!";
         } catch (IllegalArgumentException e){
-            result = "Choose valid second currency!";
-            return result;
+            return "Choose valid second currency!";
         }
 
-        return result;
+        logger.info("rate " + rateExchange.toString());
+
+        BigDecimal resultExchange = bigDecimalValue.multiply(rateExchange);
+
+        resultBuilder.append(bigDecimalValue.toString())
+                .append(currencyFrom.toString())
+                .append(" equals ")
+                .append(resultExchange)
+                .append(currencyTo);
+
+        return resultBuilder.toString();
     }
 }
--- src/main/java/com/example/controllers/FirstController.java
+++ src/main/java/com/example/controllers/FirstController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by mateu on 07.03.2017.
+ *
+ * My first Controller
+ * It will be a controller for tests
  */
 @RestController
 @RequestMapping("/world")
 public class FirstController {
 
     @RequestMapping("hello")
-    public String helloWorld(){
+    public String helloWorld() {
         return "Hello World";
+    }
+
+    @RequestMapping("/hello2")
+    public String helloWorld2() {
+        return "Hello world 2";
     }
 
 
