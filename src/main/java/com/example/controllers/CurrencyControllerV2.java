e5aa17a5e8f715d4a7a3f2bd10c9b6fc92e4e611
mdolata <mateuszdolata2@gmail.com>
2017-04-24 21:39:41 +0200

    Refactor CurrencyControllerV2

--- src/main/java/com/example/controllers/CurrencyControllerV2.java
+++ src/main/java/com/example/controllers/CurrencyControllerV2.java
@@ -1,6 +1,8 @@
 package com.example.controllers;
 
 import com.example.Service.ExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,60 +25,71 @@ public class CurrencyControllerV2 {
     @Autowired
     private ExchangeService service;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyController.class);
 
     @RequestMapping("checkService")
-    public String checkService(){
+    public String checkService() {
         Map<String, String> rates = service.getExchange().getRates();
 
-        rates.forEach((x,y) -> System.out.println(x + y));
+        rates.forEach((x, y) -> logger.info(x + y));
 
         return service.getExchange().getBase();
     }
 
     @RequestMapping("isWorking")
-    public Boolean isServiceWorking(){
+    public Boolean isServiceWorking() {
         return Boolean.TRUE;
     }
 
-    @RequestMapping("exchange/{value}")
-    public String exchangeValue(@PathVariable Long value,
-                                @RequestParam(name = "f") String from,
-                                @RequestParam(name = "t") String to){
+    @RequestMapping("exchange/{value:.+}")
+    public String exchangeValue(@PathVariable String value,
+                                @RequestParam(name = "base") String from,
+                                @RequestParam(name = "to") String to) {
+
         StringBuilder resultBuilder = new StringBuilder();
 
         Currency currencyFrom;
         Currency currencyTo;
-        BigDecimal rateExchange; // = new BigDecimal(Math.random() * 10 ).setScale(2, BigDecimal.ROUND_DOWN);
-        BigDecimal bigDecimalValue = new BigDecimal(String.valueOf(value));
-        try {
-            currencyFrom = Currency.getInstance(from.toUpperCase());
-        } catch (NullPointerException e){
-            return "Choose first currency!";
-        } catch (IllegalArgumentException e){
-            return "Choose valid first currency!";
-        }
+        BigDecimal rateExchange;
+        BigDecimal valueBD;
 
         try {
-            currencyTo = Currency.getInstance(to.toUpperCase());
-        } catch (NullPointerException e){
-            return "Choose second currency!";
-        } catch (IllegalArgumentException e){
-            return "Choose valid second currency!";
+            valueBD = new BigDecimal(value).setScale(2, BigDecimal.ROUND_DOWN);
+
+            // is that necessary to null checks?
+            // RequestParams has param named required that is default true
+            if (from == null || to == null) {
+                throw new RuntimeException();
+            }
+            currencyFrom = getCurrencyFromString(from);
+            currencyTo = getCurrencyFromString(to);
+        } catch (NumberFormatException e) {
+            return "Wrong value!";
+        } catch (IllegalArgumentException e) {
+            return "Have to choose valid currency";
+        } catch (RuntimeException e) {
+            return "Have to choose both values!Runtime";
         }
 
-        final Map<String, String> rates = service.getExchange(currencyFrom.toString(),
-                currencyTo.toString())
+        final Map<String, String> rates = service
+                .getExchange(currencyFrom.toString(), currencyTo.toString())
                 .getRates();
-        rateExchange =  new BigDecimal(rates.get(currencyTo.toString()));
 
-        BigDecimal resultExchange = bigDecimalValue.multiply(rateExchange);
+        rateExchange = new BigDecimal(rates.get(currencyTo.toString()));
+        BigDecimal exchangeResult = valueBD.multiply(rateExchange).setScale(4, BigDecimal.ROUND_DOWN);
 
-        resultBuilder.append(bigDecimalValue.toString())
-                .append(currencyFrom.toString())
+        resultBuilder.append(valueBD)
+                .append(currencyFrom)
+                .append(" for ")
+                .append(rateExchange)
                 .append(" equals ")
-                .append(resultExchange)
+                .append(exchangeResult)
                 .append(currencyTo);
 
         return resultBuilder.toString();
+    }
+
+    private Currency getCurrencyFromString(String currency) throws IllegalArgumentException {
+        return Currency.getInstance(currency.toUpperCase());
     }
 }
